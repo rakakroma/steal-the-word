6ae086f699df2e2a48ee196062dd86bb3cbd1cd9
rakakroma <102795114+rakakroma@users.noreply.github.com>
2023-01-08 10:52:40 +0800

    basic welcome page for new user

--- src/pages/Options/components/MainPage/MainPage.jsx
+++ src/pages/Options/components/MainPage/MainPage.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import { Box } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { DatabaseInfo } from '../DatabaseInfo.js';
 import { LineChartContainer } from './LineChart';
 import { PieChartContainer } from './PieChart.jsx';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2.js';
 import { RandomContext } from './RandomContext.jsx';
+import { ContextListContext } from '../../Options.jsx';
 
 export const MainPage = () => {
   // const { wordInfoTarget, setWordInfoTarget } = useContext(
@@ -17,6 +18,8 @@ export const MainPage = () => {
 };
 
 export const DefaultMainPage = () => {
+  const contextList = useContext(ContextListContext);
+  if (contextList && contextList.length === 0) return <NoDataPage />;
   return (
     <Grid2
       container
@@ -37,5 +40,41 @@ export const DefaultMainPage = () => {
         <RandomContext />
       </Grid2>
     </Grid2>
+  );
+};
+
+const NoDataPage = () => {
+  const imgPath = chrome.runtime.getURL('flame-welcome.png');
+  return (
+    <Box
+      sx={{
+        height: '84vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Typography variant="subtitle2">
+        Illustration by{' '}
+        <Link href="https://icons8.com/illustrations/author/oZpGJx8ts63Q">
+          Thierry Fousse
+        </Link>{' '}
+        from <Link href="https://icons8.com/illustrations">Ouch!</Link>
+      </Typography>
+      <img
+        src={imgPath}
+        alt="just a welcome image"
+        style={{ width: '300px', height: '300px' }}
+      />
+      <Typography variant="h3">You Haven't add any data yet</Typography>
+      <Typography variant="h6">
+        add some word and come back to see the changes 🤗
+      </Typography>
+      <Typography>
+        If you don't know how to do, how about watch the <Link>Tutorial</Link>,
+        or <Button variant="outlined">use the demo data</Button>
+      </Typography>
+    </Box>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -166,6 +166,15 @@ var options = {
         },
       ],
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets/img/flame-welcome.png',
+          to: path.join(__dirname, 'build'),
+          force: true,
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'pages', 'Options', 'index.html'),
       filename: 'options.html',
